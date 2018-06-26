7a4deebcadb68ac190d13b9adc2fff8d950d4ffa
Enderson <endersonjackson2@gmail.com>
2018-06-26 17:24:15 -0300

    add Test Sound

--- app/src/main/java/api/RetrofitConfig.java
+++ app/src/main/java/api/RetrofitConfig.java
@@ -4,7 +4,7 @@ public class RetrofitConfig {
 
     //implementation
     private static String ipEmulador = "10.0.2.2";
-    private static String ip ="10.7.116.9";
+    private static String ip ="10.51.66.249";
     public static String BASE_URL = "http://"+ip+":8080/";
     public static String getIp() {return ip;}
 }
--- app/src/main/java/com/example/ender/terracota/FragmentIter.java
+++ app/src/main/java/com/example/ender/terracota/FragmentIter.java
@@ -1,19 +1,43 @@
 package com.example.ender.terracota;
 
 import android.app.Fragment;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentIter extends Fragment {
+
+    Button bttPlay;
+    MediaPlayer mediaPlayer;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.iter_fragment,container,false);
+
+        bttPlay = view.findViewById(R.id.bttPlay);
+
+        bttPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mediaPlayer == null){
+                    mediaPlayer = MediaPlayer.create(getActivity(), R.raw.birds);
+                }
+
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }else{
+                    mediaPlayer = MediaPlayer.create(getActivity(), R.raw.birds);
+                    mediaPlayer.start();
+                }
+            }
+        });
 
         return view;
     }

2e0eaea4bc501b20b87cb9c518babc9a84dc5f7d
Enderson <endersonjackson2@gmail.com>
2018-07-03 15:28:37 -0300

    add Tela Animais e Ficha Técnica

--- app/src/main/java/com/example/ender/terracota/FragmentIter.java
+++ app/src/main/java/com/example/ender/terracota/FragmentIter.java
@@ -1,6 +1,7 @@
 package com.example.ender.terracota;
 
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,20 +9,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class FragmentIter extends Fragment {
 
     Button bttPlay;
     MediaPlayer mediaPlayer;
-
+    ImageView gifView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.iter_fragment,container,false);
 
-        bttPlay = view.findViewById(R.id.bttPlay);
+//      Gif animação do sound
+        gifView = view.findViewById(R.id.gifView);
+        Glide.with(getActivity())
+                .load(R.drawable.sounds)
+                .asGif()
+                .into(gifView);
 
+        bttPlay = view.findViewById(R.id.bttPlay);
         bttPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +48,7 @@ public class FragmentIter extends Fragment {
                 }
             }
         });
+
 
         return view;
     }
